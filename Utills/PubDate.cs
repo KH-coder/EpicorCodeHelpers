0cf94ed708b914602987169f3766ab281c5780f3
KH-coder <67957260+KH-coder@users.noreply.github.com>
2025-08-15 10:03:11 +0800

    Update PubDate.cs

--- Utills/PubDate.cs
+++ Utills/PubDate.cs
@@ -1,23 +1,24 @@
-    public class PubDate
+public class PubDate
+{
+    /// <summary>取得某月的第一天</summary>
+    public static DateTime FirstDayOfMonth(DateTime datetime)
     {
-        /// <summary>取得某月的第一天</summary>
-        public static DateTime FirstDayOfMonth(DateTime datetime)
-        {
-            return datetime.AddDays(1 - datetime.Day);
-        }
-        //// <summary>取得某月的最后一天</summary>
-        public static DateTime LastDayOfMonth(DateTime datetime)
-        {
-            return datetime.AddDays(1 - datetime.Day).AddMonths(1).AddDays(-1);
-        }
-        //// <summary>取得上个月的最后一天</summary>
-        public static DateTime LastDayOfPreviousMonth(DateTime datetime)
-        {
-            return datetime.AddDays(1 - datetime.Day).AddDays(-1);
-        }
-        //// <summary>取得下个月的第一天</summary>
-        public static DateTime FirstDayOfNextMonth(DateTime datetime)
-        {
-            return datetime.AddDays(1 - datetime.Day).AddMonths(1);
-        }
+        return datetime.AddDays(1 - datetime.Day);
     }
+    //// <summary>取得某月的最后一天</summary>
+    public static DateTime LastDayOfMonth(DateTime datetime)
+    {
+        return datetime.AddDays(1 - datetime.Day).AddMonths(1).AddDays(-1);
+    }
+    //// <summary>取得上个月的最后一天</summary>
+    public static DateTime LastDayOfPreviousMonth(DateTime datetime)
+    {
+        return datetime.AddDays(1 - datetime.Day).AddDays(-1);
+    }
+    //// <summary>取得下个月的第一天</summary>
+    public static DateTime FirstDayOfNextMonth(DateTime datetime)
+    {
+        return datetime.AddDays(1 - datetime.Day).AddMonths(1);
+    }
+}
+
